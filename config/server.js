9061279defdf31fd6cf417e469403003fb5a7229
NoorShinwari <nanoshin746@gmail.com>
2021-03-09 18:45:42 +0100

    aa

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1412d939de71958cdbc6bd5ec418c655'),
+    },
+  },
+});
